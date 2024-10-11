a6995bcffdd72e9f195506d2d3419905514734eb
jun7054 <dhakla3@gmail.com>
2024-10-11 15:21:43 +0900

    10-11

---
+++ pages/signin.js
@@ -0,0 +1,9 @@
+import Sign from "@/components/sign"
+import { Container } from "@mui/material"
+export default function signin() {
+    return (
+        <div className="modern-gradient">
+             <Sign></Sign>
+        </div>
+    )
+}
